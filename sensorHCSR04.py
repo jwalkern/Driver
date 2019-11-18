b1b2b02af164dac8fce394ab6230a3bd6b561e94
jwalkern <56714873+jwalkern@users.noreply.github.com>
2019-11-18 12:51:46 +0100

    Test sensor

--- sensorHCSR04.py
+++ sensorHCSR04.py
@@ -1,29 +1,36 @@
 import RPi.GPIO as GPIO
 import time
 
-#set GPIO pins
-sensor_trigger = 8
-sensor_echo = 10
+_HCSR04_sensor_trigger = None
+_HCSR04_sensor_echo = None
 
-#set GPIO mode (GPIO.BOARD)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(sensor_trigger, GPIO.OUT)
-GPIO.setup(sensor_echo, GPIO.IN)
+
+def init(sensor_trigger = 8, sensor_echo = 10):
+    #set GPIO pins
+    global _HCSR04_sensor_echo, _HCSR04_sensor_trigger
+    _HCSR04_sensor_trigger = sensor_trigger
+    _HCSR04_sensor_echo = sensor_echo
+
+    #set GPIO mode (GPIO.BOARD)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(sensor_trigger, GPIO.OUT)
+    GPIO.setup(sensor_echo, GPIO.IN)
 
 
 def distance():
+    global _HCSR04_sensor_echo, _HCSR04_sensor_trigger
     startTime = time.time()
     stopTime = time.time()
 
     #set trigger high and low after 20ms
-    GPIO.output(sensor_trigger, True)
+    GPIO.output(_HCSR04_sensor_trigger, True)
     time.sleep(0.00002)
-    GPIO.output(sensor_trigger, False)
+    GPIO.output(_HCSR04_sensor_trigger, False)
 
     #Mesure the start and stop time
-    while GPIO.input(sensor_echo) == 0:
+    while GPIO.input(_HCSR04_sensor_echo) == 0:
         startTime = time.time()
-    while GPIO.input(sensor_echo) == 1:
+    while GPIO.input(_HCSR04_sensor_echo) == 1:
         stopTime = time.time()
 
     #Time difference
@@ -31,6 +38,6 @@ def distance():
     #calc the distantce
     distance = (timeElapsed * 34300) / 2
     return distance
-    GPIO.cleanup()
+
 
 
--- testsensor.py
+++ testsensor.py
@@ -1,7 +1,7 @@
 from sensorHCSR04 import distance
+import RPi.GPIO as GPIO
 
 input("tryk 'enter' for at måle afstand.")
-
 dist = distance()
 print("Den målte afstand er = %.1f cm" % dist)
-
+GPIO.cleanup()
