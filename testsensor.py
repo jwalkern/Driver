8bc2c9de2ee68888d703ea1e679fd6bd39df3e94
jwalkern <56714873+jwalkern@users.noreply.github.com>
2019-11-18 12:54:12 +0100

    Test sensor

--- testsensor.py
+++ testsensor.py
@@ -1,6 +1,8 @@
 from sensorHCSR04 import distance
+from sensorHCSR04 import init
 import RPi.GPIO as GPIO
 
+init(8,10)
 input("tryk 'enter' for at måle afstand.")
 dist = distance()
 print("Den målte afstand er = %.1f cm" % dist)
