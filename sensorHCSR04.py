55e64451be009326732ebd6c597c2a91260888da
jwalkern <56714873+jwalkern@users.noreply.github.com>
2019-11-18 12:39:56 +0100

    Test sensor

--- sensorHCSR04.py
+++ sensorHCSR04.py
@@ -33,8 +33,4 @@ def distance():
     return distance
     GPIO.cleanup()
 
-input("tryk 'enter' for at måle afstand.")
 
-dist = distance()
-print("Den målte afstand er = %.1f cm" % dist)
-time.sleep(1)
