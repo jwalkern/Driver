b3d9aef6c7edb2195de5f655af00c78b8e15ca1e
jwalkern <56714873+jwalkern@users.noreply.github.com>
2019-11-20 11:26:57 +0100

    count Program

--- clickCounter.py
+++ clickCounter.py
@@ -8,8 +8,10 @@ count = 0
 while count <= 99:
     if GPIO.input(8) == 0 and check == True:
         count = count + 1
-        print("You have ignored the rule " + str(count) + " times.")
-        #time.sleep(0.10)
+        if count < 98:
+            print("You have ignored the rule " + str(count) + " times.")
+        else:
+            print("That is it! BYE!")
         check = False
     if GPIO.input(8) == 1:
         check = True
