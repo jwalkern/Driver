8043006d9e91a6713d78ee5562ac343342e7e9e2
jwalkern <56714873+jwalkern@users.noreply.github.com>
2019-11-20 11:03:10 +0100

    iopfejap

--- clickCounter.py
+++ clickCounter.py
@@ -3,13 +3,13 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.IN)
-
+"""
 for index in range(2):
     input()
     test = GPIO.input(8)
     print(test)
+"""
 
-check = True
 count = 0
 while count <= 10:
     if GPIO.input(8) == 0 and check == True:
@@ -17,7 +17,8 @@ while count <= 10:
         print("Du har trykket " + str(count) + " gange.")
         time.sleep(0.10)
         check = False
-    check = True
+    if GPIO.input(8) == 1:
+        check = True
 
 
 
