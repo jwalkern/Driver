48dffb0ef102fd1c8c2973c8b7e4b4947d4f5010
jwalkern <56714873+jwalkern@users.noreply.github.com>
2019-11-20 11:29:03 +0100

    count Program

--- clickCounter.py
+++ clickCounter.py
@@ -1,14 +1,14 @@
 import RPi.GPIO as GPIO
-
+import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.IN)
 
 print("DO NOT PUSH THE BUTTON!")
 count = 0
-while count <= 99:
+while count <= 9:
     if GPIO.input(8) == 0 and check == True:
         count = count + 1
-        if count < 98:
+        if count < 9:
             print("You have ignored the rule " + str(count) + " times.")
         else:
             print("That is it! BYE!")
