77c99cc2db325ef55bf6bff2cfa40632ebfe3719
jwalkern <56714873+jwalkern@users.noreply.github.com>
2019-11-20 11:12:16 +0100

    count Program

--- clickCounter.py
+++ clickCounter.py
@@ -11,13 +11,10 @@ for index in range(2):
 """
 print("DO NOT PUSH THE BUTTON!")
 count = 0
-while count <= 100:
+while count <= 99:
     if GPIO.input(8) == 0 and check == True:
         count = count + 1
-        if count > 10:
-            print("I have told you " + str(count) + " times, not to push the button.")
-        else:
-            print("Congrats you have pushed the button " + str(count) + " times.")
+        print("You have pushed the button " + str(count) + " times.")
         #time.sleep(0.10)
         check = False
     if GPIO.input(8) == 1:
