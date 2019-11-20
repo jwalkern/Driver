5503c4e6ddab3339232c6eda2a5f45d3ed786a9f
jwalkern <56714873+jwalkern@users.noreply.github.com>
2019-11-20 12:52:13 +0100

    count Program

--- clickCounter.py
+++ clickCounter.py
@@ -3,22 +3,18 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(8, GPIO.FALLING)
+GPIO.add_event_detect(8, GPIO.FALLING, bouncetime=200)
 
 print("DO NOT PUSH THE BUTTON!")
 count = 0
 while count <= 9:
-    if GPIO.event_detected(8): #and check == True:
+    if GPIO.event_detected(8):
         count = count + 1
         if count < 10:
             print("You have ignored the rule " + str(count) + " times.")
         else:
-            count == 10
             print("That is it! BYE!")
-        #check = False
         time.sleep(0.1)
-    #if GPIO.input(8) == 1:
-        #check = True
 GPIO.cleanup()
 
 
